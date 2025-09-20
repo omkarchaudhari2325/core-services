5fd0b346993f666093f1aace39736197335aa5fb
omkarchaudhari2325 <chaudhariomkar369@gmail.com>
2025-09-20 15:32:27 +0530

    adding header once more

--- core-front-end/src/components/header/Header.jsx
+++ core-front-end/src/components/header/Header.jsx
@@ -4,6 +4,8 @@ const Header = () => {
   return (
     <div>
       <h2>This is the header component</h2>
+      <h2>This is the other header then</h2>
+      <h1>This is the other other header</h1>
     </div>
   );
 };
